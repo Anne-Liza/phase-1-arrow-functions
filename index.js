8c90b60251cf08e53c7512a33a909ce01752fcb5
Anne <annewangui210@gmail.com>
2024-07-07 19:09:43 +0300

    Arrow functions lab

---
+++ index.js
@@ -0,0 +1,16 @@
+  // declare a function using arrow function
+  const add = (parameter1, parameter2) => parameter1+parameter2;
+  add(2,3);
+  console.log(add(2,3));
+
+  const divide = (parameter1, parameter2) => parameter1/parameter2;
+  divide(2000,100);
+  console.log(divide(2000,100));
+
+ 
+  const square = parameter => parameter*parameter;
+  square(2);
+  console.log(square(2));
+
+
+  
